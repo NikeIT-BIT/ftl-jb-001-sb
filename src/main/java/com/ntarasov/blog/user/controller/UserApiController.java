ed5938a490c77a1d5af8df49a3ec4d0eb0e4124d
Никита Тарасов` <nikita.taras0ff@yandex.ru>
2022-07-14 22:58:31 +0300

    API Full Users

--- src/main/java/com/ntarasov/blog/user/controller/UserApiController.java
+++ src/main/java/com/ntarasov/blog/user/controller/UserApiController.java
@@ -5,12 +5,16 @@ import com.ntarasov.blog.user.api.response.UserFullResponse;
 import com.ntarasov.blog.user.api.response.UserResponse;
 import com.ntarasov.blog.user.exception.UserExistException;
 import com.ntarasov.blog.user.mapping.UserMapping;
+import com.ntarasov.blog.user.model.UserDoc;
 import com.ntarasov.blog.user.routers.UserApiRoutes;
 import com.ntarasov.blog.user.service.UserApiService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +22,8 @@ public class UserApiController {
     private final UserApiService userApiService;
 
     @PostMapping(UserApiRoutes.ROOT)
-    public UserResponse registration(@RequestBody RegistrationRequest request) throws UserExistException {
-        return UserMapping.getInstance().getResponse().convert(userApiService.registration(request));
+    public UserFullResponse registration(@RequestBody RegistrationRequest request) throws UserExistException {
+        return UserMapping.getInstance().getResponseFull().convert(userApiService.registration(request));
     }
     @GetMapping(UserApiRoutes.BY_ID)
    public UserFullResponse byId(@PathVariable ObjectId id) throws ChangeSetPersister.NotFoundException {
@@ -27,5 +31,13 @@ public class UserApiController {
                 userApiService.findById(id).orElseThrow(
                         ChangeSetPersister.NotFoundException::new)
         );
+   }
+
+   @GetMapping(UserApiRoutes.ROOT)
+    public List<UserResponse> search(){
+        return UserMapping.getInstance().getSearch().convert(
+                userApiService.search()
+        );
+
    }
 }
